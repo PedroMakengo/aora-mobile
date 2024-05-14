82fc364daaa01a1226f41a22ce6fff721c296adf
PedroMakengo <pedromakengomiguel@gmail.com>
2024-05-14 21:42:39 +0100

    Resolvido o erro

--- components/EmojiList/index.js
+++ components/EmojiList/index.js
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import { FlatList, StyleSheet, Image, Pressable } from 'react-native'
+import { FlatList, StyleSheet, Platform, Image, Pressable } from 'react-native'
 
 export default function EmojiList({ onSelect, onCloseModal }) {
   const [emoji] = useState([
-    require('../assets/images/emoji1.png'),
-    require('../assets/images/emoji2.png'),
-    require('../assets/images/emoji3.jpg'),
-    require('../assets/images/emoji4.jpg'),
-    require('../assets/images/emoji5.png'),
+    require('../../assets/images/emoji1.png'),
+    require('../../assets/images/emoji2.png'),
+    require('../../assets/images/emoji3.jpg'),
+    require('../../assets/images/emoji4.jpg'),
+    require('../../assets/images/emoji5.png'),
   ])
   return (
     <FlatList
