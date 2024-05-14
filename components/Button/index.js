b77a94c1546108e4f7cf2d85a8c2041ce384c8f5
PedroMakengo <pedromakengomiguel@gmail.com>
2024-05-14 15:48:43 +0100

    Adicionado mais elementos a minha estrutura

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,6 +1,33 @@
 import { StyleSheet, View, Pressable, Text } from 'react-native'
+import FontAwesome from '@expo/vector-icons/FontAwesome'
 
-export default function Button({ label }) {
+export default function Button({ label, theme, onPress }) {
+  if (theme === 'primary') {
+    return (
+      <View
+        style={[
+          styles.buttonContainer,
+          { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
+        ]}
+      >
+        <Pressable
+          style={[styles.button, { backgroundColor: '#fff' }]}
+          onPress={onPress}
+        >
+          <FontAwesome
+            name="picture-o"
+            size={18}
+            color="#25292e"
+            style={styles.buttonIcon}
+          />
+          <Text style={[styles.buttonLabel, { color: '#25292e' }]}>
+            {label}
+          </Text>
+        </Pressable>
+      </View>
+    )
+  }
+
   return (
     <View style={styles.buttonContainer}>
       <Pressable
@@ -15,7 +42,7 @@ export default function Button({ label }) {
 
 const styles = StyleSheet.create({
   buttonContainer: {
-    width: 320,
+    width: 320, // 320
     height: 68,
     marginHorizontal: 20,
     alignItems: 'center',
